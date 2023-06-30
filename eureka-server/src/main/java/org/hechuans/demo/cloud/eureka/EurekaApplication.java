bbdf7fe87aea740c0cfe785846ef0f57f908ccc8
hechuansheng <4rfv$RFV>
2023-06-30 14:31:44 +0800

    Initial commit

---
+++ eureka-server/src/main/java/org/hechuans/demo/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package org.hechuans.demo.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : hechuansheng
+ * @date : 2023/6/30 14:18
+ * @since : version-1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
